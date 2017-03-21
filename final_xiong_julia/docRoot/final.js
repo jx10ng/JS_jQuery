b101191f1d3b2138bfcc25f4ac34485a5a8c25b7
Julia Xiong <julxiong@gmail.com>
2017-03-21 12:41:15 -0700

    Final added

---
+++ final_xiong_julia/docRoot/final.js
@@ -0,0 +1,149 @@
+var divReg;
+var divMain;
+//comment added	
+document.addEventListener("DOMContentLoaded", function(){
+	
+	//for #divRegistered
+	divReg = document.getElementById("divRegistered");
+	//for #divMain
+	divMain = document.getElementById("divMain");
+
+	//Any element in divMain with focus will have background-Color of yellow
+	divMain.addEventListener("focus", fHandleEnter, true);
+	//Any element in divMain with blur will return background-Color to white
+	divMain.addEventListener("blur", fHandleExit, true);
+
+	//array of all input elements in the form that have attribute type = text or type = password
+	var inputElements = document.getElementById("frmRegister").querySelectorAll("input[type='password'],[type='text']");
+	//for testing: display alert with id of each inputElements element
+	/*
+	for (i = 0; i < inputElements.length; i++){
+		alert("inputElement(" + i + "): " + inputElements[i].id);
+	}
+	*/
+
+	//array of all span elements in the form 
+	var spanElements = document.getElementById("frmRegister").querySelectorAll("span");
+	//for testing: display alert with id of each spanElement element
+	/*
+	for (i = 0; i < spanElements.length; i++){
+		alert("spanElements(" + i + "): " + spanElements[i].id);
+	}
+	*/
+
+	//event handler for login with min length = 7 and max = 15
+	inputElements[0].addEventListener("blur", function(){
+		fCheckLogin(inputElements[0].value,spanElements[0]);
+	});
+
+	//event handler for password, compares password values after blur on second value
+	inputElements[2].addEventListener("blur", function(){
+		fCompareInput(inputElements[1].value, inputElements[2].value, spanElements[2]);
+	});
+
+	//event handler for email, compares email values after blur on second value
+	inputElements[4].addEventListener("blur", function(){
+		fCompareInput(inputElements[3].value, inputElements[4].value, spanElements[4]);
+	});
+
+	fProcessForm()
+});
+
+
+//function for changing style background-color to yellow
+function fHandleEnter(e){
+	e.target.style.backgroundColor = "yellow";
+}
+
+//function for changing style background-color to white
+function fHandleExit(e){
+	e.target.style.backgroundColor = "white";
+}
+
+//function for form submission
+function fProcessForm(){
+	//best to use decodeURIComponent even if location.search is simple, may have special characters
+	var strQueryString = decodeURIComponent(location.search); 
+	
+	//alert(strQueryString);
+	//alert("query string length: " + strQueryString.length);
+	//?Login=aa&Password=bb&Confirm_Password=cc&Email=dd&Confirm_Email=ee
+
+	//reqExp to find the Login
+	var loginExp = /Login=(\w*)&/;
+
+	//use non-global match() for login
+	var loginMatch = strQueryString.match(loginExp); 
+
+	//if statement just in case the loginMatch is undefined
+	if (loginMatch){
+		//alert("login is: " + matchtext[1]); 
+		//match() with non-global returns entire match and individual matches in array, to get the individual use array position ex.[1].
+		var login = loginMatch[1];
+		
+		//check to see if query string is > 0
+		if (strQueryString.length > 0) {
+			//hide the #divMain div
+			$(divMain).hide();
+			//show the #divRegister div slowly
+		    $(divReg).fadeIn(1000);
+		    //append text to divRegister
+		    $(divReg).append("Thank you, " + login + ". You are now registered.");
+		}
+		//check to see if query string is == 0
+		if (strQueryString.length == 0) {
+			//hide the #divMain div
+			$(divMain).fadeIn();
+			//show the #divRegister div
+		    $(divReg).hide();
+		}
+	}
+}
+
+//function to compare Email and Password input values
+function fCompareInput(value1, value2, display){
+	//if at least one value is empty
+	if (value1.length == 0 || value2.length == 0) {
+		display.innerHTML = "Missing Entry"; 
+		display.style = "";
+	}
+	//if both value1 and value2 exist
+	else {
+		//if value1 and value2 are equal
+		if (value1==value2) {
+			display.innerHTML = "Entries match";
+			display.style.backgroundColor = "green";
+		}
+		//if value1 and value2 are  NOT equal
+		else {
+			display.innerHTML = "Entries do not match";
+			display.style.backgroundColor = "red";
+
+		}
+	}
+
+}
+
+//function to check length requirement 7-15 characters
+function fCheckLogin(value0, display){
+	//if login length < 7
+	if (value0.length < 7) {
+		display.innerHTML = "Length: " + value0.length + ". Need 7-15 characters";
+		display.style.backgroundColor = "red";
+	}
+	//if login length >15
+	if (value0.length > 15) {
+		display.innerHTML = "Length: " + value0.length + ". Need 7-15 characters";
+		display.style.backgroundColor = "red";
+	}
+	//if login is between 7-15 characters
+	if (value0.length >= 7 && value0.length <= 15) {
+		display.innerHTML = "Valid";
+		display.style.backgroundColor = "green";
+	}
+
+}
+
+
+
+
